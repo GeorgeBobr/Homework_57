39da6cddb90041e3c1dbc7ddc45fa56576aeae75
Georg <georgestoneix@gmail.com>
2023-12-14 07:20:11 -0800

    Написал форму типа, статуса и задачи

---
+++ webapp/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.forms import widgets
+
+from webapp.models import Type
+from webapp.models import Status
+
+class TypeForm(forms.Form):
+    title = forms.CharField(max_length=50, required=True, label="Название")
+
+class StatusForm(forms.Form):
+    title = forms.CharField(max_length=50, required=True, label="Название")
+
+class TaskForm(forms.Form):
+    summary = forms.CharField(max_length=50, required=True, label="Заголовок")
+    description = forms.CharField(max_length=2000, required=True, label="Описание",
+                                  widget=widgets.Textarea(attrs={"cols": 30, "rows": 5, "class": "test"}))
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label="Типы")
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), label="Статусы")
