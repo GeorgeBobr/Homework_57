c823f78ccd2d50eb53d82257cc507e1c2782ffa2
Georg <georgestoneix@gmail.com>
2023-12-17 03:07:22 -0800

    Создал класс NoSpecificCharactersValidator, также создал функцию def at_least_5 Добавил валидаторы описания и заголовка в класс TaskForm:

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import widgets
-    from django.core.validators import BaseValidator
+from django.core.validators import BaseValidator
 from django.core.exceptions import ValidationError
 
 from webapp.models import Type
@@ -12,16 +12,28 @@ def validate_summary(value):
 
 def validate_description(value):
     if len(value) < 10:
-        raise ValidationError("Ваше описание должно содержать не менее 10 символов")
+        raise ValidationError("Описание задачи должно содержать не менее 10 символов")
     elif "bad_word" in value.lower():
         raise ValidationError("Описание не должно содержать бранных слов")
 
-class No_specific_c
+class NoSpecificCharactersValidator(BaseValidator):
+    message = "Заголовок не должен содержать определенные символы"
+    code = "no_specific_characters"
+
+    def compare(self, value, initial):
+        forbidden_characters = ['@', '#', '$', '^']
+        return not any(char in forbidden_characters for char in str(value))
+
+def at_least_5(value):
+    if len(value) < 5:
+        raise ValidationError('Заголовок должен содержать не менее 5 символов')
 
 class TaskForm(forms.Form):
-    summary = forms.CharField(max_length=50, required=True, label="Заголовок")
+    summary = forms.CharField(max_length=50, required=True, label="Заголовок",
+                              validators=[validate_summary, at_least_5])
     description = forms.CharField(max_length=2000, required=False, label="Описание",
-                                  widget=widgets.Textarea(attrs={"cols": 30, "rows": 5, "class": "test"}))
+                                  widget=widgets.Textarea(attrs={"cols": 30, "rows": 5, "class": "test"}),
+                                  validators=[validate_description])
     types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label="Типы")
     status = forms.ModelChoiceField(queryset=Status.objects.all(), label="Статусы")
 
