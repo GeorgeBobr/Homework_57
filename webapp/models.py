c88c4be2c965e0ab94a99eafa2237472f04e5cc2
Georg <georgestoneix@gmail.com>
2023-12-21 03:52:40 -0800

    Создал класс проект, а также связал его внешним ключем с классом Task

--- webapp/models.py
+++ webapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Type(models.Model):
     title = models.CharField(max_length=50, verbose_name="Название", unique=True, null=False, blank=False)
@@ -21,6 +22,12 @@ class Status(models.Model):
         verbose_name = "Статус"
         verbose_name_plural = "Статусы"
 
+class Project(models.Model):
+    start_data = models.DateField()
+    end_data = models.DateField(null=True, blank=True)
+    title = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+
 class Task(models.Model):
     summary = models.CharField(max_length=50, verbose_name="Заголовок", unique=True, null=False, blank=False)
     description = models.TextField(max_length=2000, blank=True, null=True, verbose_name='Описание')
@@ -28,7 +35,7 @@ class Task(models.Model):
     types = models.ManyToManyField(Type)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
-
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, blank=True)
     def __str__(self):
         return f"{self.id} {self.summary}"
 
@@ -36,3 +43,4 @@ class Task(models.Model):
         db_table = "Task"
         verbose_name = "Задача"
         verbose_name_plural = "Задачи"
+
