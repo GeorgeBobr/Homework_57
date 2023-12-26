f5ed489386a3721a4c52fca6851ebf5c5577675f
Georg <georgestoneix@gmail.com>
2023-12-26 13:50:33 -0800

    создал urls для Project

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
-
-
+from .views.views import TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
+from .views.project_views import (ProjectListView, ProjectDetailView, ProjectCreateView, ProjectUpdateView,
+                                  ProjectDeleteView)
+app_name = 'webapp'
 urlpatterns = [
-    path('', TaskListView.as_view(), name='task_list'),
-    path('task/<int:id>/', TaskDetailView.as_view(), name='detail'),
+    path('', ProjectListView.as_view(), name='project_list'),
+    path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('projects/create/', ProjectCreateView.as_view(), name='project_create'),
+    path('projects/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
+    path('projects/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
+
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='detail'),
     path('task/create/', TaskCreateView.as_view(), name='create'),
-    path('task/update/<int:id>/', TaskUpdateView, name='update'),
-    path('task/delete/<int:id>/', TaskDeleteView, name='delete')
+    path('task/update/<int:pk>/', TaskUpdateView.as_view(), name='update'),
+    path('task/delete/<int:pk>/', TaskDeleteView.as_view(), name='delete')
 ]
