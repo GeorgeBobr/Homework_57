981674e77004aefa3665400b3b4a5302b45a18ed
Georg <georgestoneix@gmail.com>
2023-12-26 17:48:37 -0800

    Добваил url. Создание задачи в выбранном проекте

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,13 +5,15 @@ from .views.project_views import (ProjectListView, ProjectDetailView, ProjectCre
 app_name = 'webapp'
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
+
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('project/<int:pk>/task/create/', TaskCreateView.as_view(), name='create'),
+
     path('projects/create/', ProjectCreateView.as_view(), name='project_create'),
     path('projects/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
     path('projects/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
 
     path('task/<int:pk>/', TaskDetailView.as_view(), name='detail'),
-    path('task/create/', TaskCreateView.as_view(), name='create'),
     path('task/update/<int:pk>/', TaskUpdateView.as_view(), name='update'),
     path('task/delete/<int:pk>/', TaskDeleteView.as_view(), name='delete')
 ]
