490767f5f88ec9f53f303468710733fae1a41499
Georg <georgestoneix@gmail.com>
2024-01-08 07:08:02 -0800

    добавил ссылку на просмотр пользователей проекта

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views.views import TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
 from .views.project_views import (ProjectListView, ProjectDetailView, ProjectCreateView, ProjectUpdateView,
-                                  ProjectDeleteView)
+                                  ProjectDeleteView, ProjectUsersView)
 app_name = 'webapp'
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
 
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
     path('project/<int:pk>/task/create/', TaskCreateView.as_view(), name='create'),
+    path('<int:pk>/users/', ProjectUsersView.as_view(), name='project_users'),
 
     path('projects/create/', ProjectCreateView.as_view(), name='project_create'),
     path('projects/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
