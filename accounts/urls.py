e0d99d8d07cdf8d4f0ce2c8994e3116880eccc88
Georg <georgestoneix@gmail.com>
2024-01-08 07:10:08 -0800

    создал путь для регистрации

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from accounts.views import login_view, logout_view
 from django.contrib.auth.views import LoginView, LogoutView
+from accounts.views import RegisterView
 
 app_name = 'accounts'
 
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='registration')
 ]
