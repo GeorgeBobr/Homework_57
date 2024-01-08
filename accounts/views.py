6805d7a091a7e729baabfe09255f45389a72e988
Georg <georgestoneix@gmail.com>
2024-01-08 07:10:25 -0800

    создал класс регистарции

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,22 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.views.generic import CreateView
+from accounts.forms import MyUserCreationForm
+class RegisterView(CreateView):
+    model = get_user_model()
+    form_class = MyUserCreationForm
+    template_name = 'accounts/registration.html'
 
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
 
-def login_view(request):
-    context = {}
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('webapp:project_list')
-        else:
-            context['has_error'] = True
-    return render(request, 'login.html', context=context)
+    def get_success_url(self):
+        next_page = self.request.GET.get('next')
+        if not next_page:
+            next_page = self.request.POST.get('next')
+        if not next_page:
+            next_page = reverse('webapp:project_list')
 
-
-def logout_view(request):
-    logout(request)
-    return redirect('webapp:project_list')
+        return next_page
