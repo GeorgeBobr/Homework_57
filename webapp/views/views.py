9ae0e0fcc1b2507b0eba0fe1ba6b9238dc922db7
Georg <georgestoneix@gmail.com>
2024-01-09 05:49:05 -0800

    Добавил Loginrequiredmixin и PermissionREquiedmixin

--- webapp/views/views.py
+++ webapp/views/views.py
@@ -1,5 +1,5 @@
 from audioop import reverse
-
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView, UpdateView, DeleteView, DetailView, CreateView
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 from webapp.forms import TaskForm
 from webapp.models import Task, Project
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     template_name = 'tasks/create.html'
     form_class = TaskForm
@@ -16,20 +16,36 @@ class TaskCreateView(CreateView):
         project = get_object_or_404(Project, pk=self.kwargs.get('pk'))
         task = form.save(commit=False)
         task.project = project
+        task.users = self.request.user
         task.save()
         return redirect('webapp:project_detail', pk=project.pk)
 
     def get_success_url(self):
         return reverse('webapp:project_detail', kwargs={'pk': self.kwargs['pk']})
 
-class TaskUpdateView(UpdateView):
+class TaskUpdateView(PermissionRequiredMixin, UpdateView):
     template_name = 'tasks/update.html'
     model = Task
     form_class = TaskForm
-class TaskDeleteView(DeleteView):
+    permission_required = 'webapp.edit_task'
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().author
+
+    def get_success_url(self):
+        return reverse('webapp:article_view', kwargs={'pk': self.object.article.pk})
+
+class TaskDeleteView(PermissionRequiredMixin, DeleteView):
     model = Task
     template_name = 'tasks/delete.html'
-    success_url = reverse_lazy('project_detail')
+
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().author
+
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse('webapp:project_detail', kwargs={'pk': self.object.project.pk})
 
 class TaskDetailView(TemplateView):
     model = Task
